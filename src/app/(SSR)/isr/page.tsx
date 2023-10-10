79934a67743cb8e725e8f0984e6f93b0eb0bc9c7
Anthony Tleiji <anthonytleiji@gmail.com>
2023-10-10 17:30:28 +0300

    Added ISR in server component.

---
+++ src/app/(SSR)/isr/page.tsx
@@ -0,0 +1,30 @@
+import { Alert } from "@/components/bootstrap";
+import { UnsplashImage } from "@/models/unsplash-image";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata = {
+    title: 'Incremental Static Regeneration - NextJS 13.4 Image Gallery Demo',
+}
+
+export const revalidate = 30;
+
+export default async function ISRPage() {
+    const response = await fetch("https://api.unsplash.com/photos/random?client_id=" + process.env.UNSPLASH_ACCESS_KEY);
+    const image: UnsplashImage = await response.json();
+
+    const width = Math.min(500, image.width);
+    const height = (width / image.width) * image.height;
+
+    return (
+        <div className="d-flex flex-column algin-items-center">
+            <Alert>
+                This page uses <strong>incremental static regeneration</strong>.
+                <br />
+                A new image is fetched every 30 seconds (after refreshing the page) and then served from cache for that duration.
+            </Alert>
+            <Image src={image.urls.raw} width={width} height={height} alt={image.description} className="rounded shadow mw-100 h-100" />
+            by <Link href={"/users/" + image.user.username}>{image.user.username}</Link>
+        </div>
+    );
+}
